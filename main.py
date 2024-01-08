73d41750bf8f8865580e3a4fd3c8a85c8188a539
silenc3502 <gcccompil3r@gmail.com>
2024-01-09 01:35:29 +0900

    feat: [이상훈] Main Window Entity 구성 [TCG-CGU-1]

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("Hello")
+if __name__ == "__main__":
+    pass
---
+++ tests/main_window/entity/window_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from main_window.entity.window import Window
+
+
+class TestWindowClass(unittest.TestCase):
+
+    def test_default_values(self):
+        window = Window()
+        self.assertEqual(window.get_title(), "")
+        self.assertEqual(window.get_geometry(), "800x600")
+        self.assertEqual(window.get_background_color(), "#FFFFFF")
+        self.assertEqual(window.get_resizable(), (True, True))
+
+    def test_set_values(self):
+        window = Window("Custom Title", "1024x768", "#00FF00", (False, True))
+
+        self.assertEqual(window.get_title(), "Custom Title")
+        self.assertEqual(window.get_geometry(), "1024x768")
+        self.assertEqual(window.get_background_color(), "#00FF00")
+        self.assertEqual(window.get_resizable(), (False, True))
+
+if __name__ == '__main__':
+    unittest.main()
