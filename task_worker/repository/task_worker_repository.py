a6d64567401717ff4c651e87905a788d0d74b456
silenc3502 <gcccompil3r@gmail.com>
2024-01-10 00:26:55 +0900

    feat: [이상훈] 원하는 시점에서 등록한 함수를 동작시키는 Task 구동 [TCG-CGU-9]

--- task_worker/repository/task_worker_repository.py
+++ task_worker/repository/task_worker_repository.py
@@ -5,3 +5,7 @@ class TaskWorkerRepository(abc.ABC):
     @abc.abstractmethod
     def save_task_worker(self, name, will_be_execute_function):
         pass
+
+    @abc.abstractmethod
+    def execute_task(self, name):
+        pass
--- task_worker/repository/task_worker_repository_impl.py
+++ task_worker/repository/task_worker_repository_impl.py
@@ -1,3 +1,5 @@
+import multiprocessing
+
 from task_worker.entity.task_worker import TaskWorker
 from task_worker.repository.task_worker_repository import TaskWorkerRepository
 
@@ -18,7 +20,21 @@ class TaskWorkerRepositoryImpl(TaskWorkerRepository):
         return cls.__instance
 
     def save_task_worker(self, name, will_be_execute_function):
+        print("TaskWorkerRepositoryImpl: save_task_worker()")
         task_worker = TaskWorker(name, will_be_execute_function)
         self.__thread_worker_list[name] = task_worker
+
+    def execute_task(self, name):
+        print("TaskWorkerRepositoryImpl: execute_task()")
+        found_named_task_worker = self.__thread_worker_list[name]
+        execute_function = found_named_task_worker.getWillBeExecuteFunction()
+
+        newTask = multiprocessing.Process(target=execute_function, args=(None,))
+        print(f"newTask: {newTask}")
+        found_named_task_worker.setTaskPid(newTask.pid)
+
+        newTask.start()
+        newTask.join()
+
 
 
