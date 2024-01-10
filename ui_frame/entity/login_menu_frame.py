b24b995b93f9c801cb8015315226e60dcce43380
silenc3502 <gcccompil3r@gmail.com>
2024-01-11 00:06:17 +0900

    chore: [이상훈] remove useless [TCG-CGU-13]

--- ui_frame/entity/login_menu_frame.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-
-
-class LoginMenuFrame(tkinter.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.configure(bg="#000000")
