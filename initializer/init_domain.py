98dcb9a79f4a524d42dc3d00ed836ecdf2e3ce35
Sanghoon Lee <gcccompil3r@gmail.com>
2024-01-11 17:11:04 +0900

    update: [이상훈] Socket Domain 초기화 [TCG-CGU-10]

--- initializer/init_domain.py
+++ initializer/init_domain.py
@@ -1,4 +1,5 @@
 from app_window.service.window_service_impl import WindowServiceImpl
+from client_socket.service.client_socket_service_impl import ClientSocketServiceImpl
 from login_frame.service.login_menu_frame_service_impl import LoginMenuFrameServiceImpl
 from main_frame.service.main_menu_frame_service_impl import MainMenuFrameServiceImpl
 from task_worker.service.task_worker_service_impl import TaskWorkerServiceImpl
@@ -24,6 +25,10 @@ class DomainInitializer:
         LoginMenuFrameServiceImpl.getInstance()
 
     @staticmethod
+    def initClientSocketDomain():
+        ClientSocketServiceImpl.getInstance()
+
+    @staticmethod
     def initUiFrameDomain():
         UiFrameControllerImpl.getInstance()
 
@@ -33,6 +38,7 @@ class DomainInitializer:
         DomainInitializer.initMainMenuFrameDomain()
         DomainInitializer.initLoginMenuFrameDomain()
         DomainInitializer.initUiFrameDomain()
+        DomainInitializer.initClientSocketDomain()
         DomainInitializer.initTaskWorkerDomain()
 
 
