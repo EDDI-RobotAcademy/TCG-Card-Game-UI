f5defb82c2a2d9bc942392b9f8bcaa272f7deb0c
LoHenP <qwpoal119@naver.com>
2024-01-18 18:14:28 +0900

    feat: [이재승] 게임 플레이어가 로비 화면에서 대전 입장 버튼을 누르면 전투 대기실 화면이 나타납니다 [TCG-CGU-23]

--- battle_lobby_frame/repository/battle_lobby_frame_repository_impl.py
+++ battle_lobby_frame/repository/battle_lobby_frame_repository_impl.py
@@ -1,9 +1,12 @@
+import tkinter
+
 from battle_lobby_frame.entity.battle_lobby_frame import BattleLobbyFrame
 from battle_lobby_frame.repository.battle_lobby_frame_repository import BattleLobbyFrameRepository
 
 
 class BattleLobbyFrameRepositoryImpl(BattleLobbyFrameRepository):
     __instance = None
+    __decks = []
 
     def __new__(cls):
         if cls.__instance is None:
@@ -21,3 +24,23 @@ class BattleLobbyFrameRepositoryImpl(BattleLobbyFrameRepository):
         battleLobbyFrame = BattleLobbyFrame(rootWindow)
 
         return battleLobbyFrame
+
+    def getDeckList(self):
+        return self.__decks
+
+    def addDeckToDeckList(self, deck):
+        self.__decks.append(deck)
+        print(f"decks: {self.__decks}")
+
+    def setDeckToDeckList(self,i,deck):
+        self.__decks[i] = deck
+
+    def removeDeckFromDeckList(self, deck):
+        self.__decks.remove(deck)
+
+    def selectDeck(self, deck: tkinter.Button):
+        for d in self.__decks:
+            if d==deck:
+                deck.configure(bg="#336633")
+            else:
+                d.configure(bg="#ffffff")
