9525e956548da76a818be9b5aa08abc11011805a
silenc3502 <gcccompil3r@gmail.com>
2024-01-20 00:23:08 +0900

    feat: [이상훈] 메인 메뉴에서 종료 요청하기 [TCG-CGU-39]

---
+++ main_frame/service/request/program_exit_request.py
@@ -0,0 +1,14 @@
+from common.protocol import CustomProtocol
+
+
+class ProgramExitRequest:
+    def __init__(self):
+        self.__protocolNumber = CustomProtocol.PROGRAM_EXIT.value
+
+    def toDictionary(self):
+        return {
+            "protocolNumber": self.__protocolNumber,
+        }
+
+    def __str__(self):
+        return f"AccountRegisterRequest(protocolNumber={self.__protocolNumber})"
