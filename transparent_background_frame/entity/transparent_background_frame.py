e1575a734706aea894b7324a7d4fd95c2cdd9080
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2024-01-22 13:31:25 +0900

    refactor:[이재림] 투명 배경 생성을 위해 만들었던 불필요한 코드 삭제

--- transparent_background_frame/entity/transparent_background_frame.py
+++
@@ -1,7 +0,0 @@
-import tkinter
-
-class TransparentBackgroundFrame(tkinter.Frame):
-    def __init__(self, master=None, **kwargs):
-        super().__init__(master, **kwargs)
-        self.canvas = tkinter.Canvas(self, width=1200, height=800, bg="white", highlightthickness=0)
-        self.canvas.pack(expand=True, fill="both")
