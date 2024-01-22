23eae52fa61cfeceb4658e99be673b5a0d346342
LoHenP <qwpoal119@naver.com>
2024-01-22 15:23:45 +0900

    feat: [이재승] 로비 메뉴에서 대전 입장을 눌러 배틀 로비로 입장 할 때, 서버로부터 내 덱의 정보를 받아옵니다. [TCG-CGU-78]

--- lobby_frame/service/lobby_menu_frame_service.py
+++ lobby_frame/service/lobby_menu_frame_service.py
@@ -5,3 +5,10 @@ class LobbyMenuFrameService(abc.ABC):
     @abc.abstractmethod
     def createLobbyUiFrame(self, rootWindow, switchFrameWithMenuName):
         pass
+
+    @abc.abstractmethod
+    def injectTransmitIpcChannel(self, transmitIpcChannel):
+        pass
+    @abc.abstractmethod
+    def injectReceiveIpcChannel(self, receiveIpcChannel):
+        pass
---
+++ lobby_frame/service/request/enter_battle_lobby_request.py
@@ -0,0 +1,5 @@
+class EnterBattleLobbyRequest:
+    __sessionId = None
+
+    def __init__(self, sessionId):
+        self.__sessionId = sessionId
