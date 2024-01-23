952acca4f970f185a9636f8ed5eaad0e71c756e6
Jungpop <shockstrut22@gmail.com>
2024-01-23 16:32:30 +0900

    MyDeckFrame을 tkinter에서 OpenGL로 이미지 표현 방식을 변경합니다.[TCG-CGU-88]

---
+++ my_deck_frame/entity/my_deck_frame_scene.py
@@ -0,0 +1,12 @@
+class MyDeckFrameScene:
+    def __init__(self):
+        self.shapes = []
+        self.images = []
+        self.translations = []
+
+    def add_shape(self, shape, translation=(0, 0)):
+        self.shapes.append(shape)
+        self.translations.append(translation)
+
+    def add_image(self, image):
+        self.images.append(image)
