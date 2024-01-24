bdedaa1c46f1948824506483b10af136c827910e
Jungpop <shockstrut22@gmail.com>
2024-01-24 15:13:41 +0900

    feat: [조정환] 게임플레이어가 in game에서 battle field를 볼 수 있는 frame을 생성합니다.[TCG-CGU-61]

--- opengl_battle_field/entity/battle_field.py
+++ opengl_battle_field/entity/battle_field.py
@@ -1,6 +1,7 @@
 class BattleField:
     def __init__(self):
         self.unit_card = []
+        self.tomb = []
 
     def get_unit_card(self):
         return self.unit_card
@@ -8,5 +9,8 @@ class BattleField:
     def add_unit_card(self, unit_card):
         self.unit_card.append(unit_card)
 
+    def get_tomb(self):
+        return self.tomb
 
-
+    def add_tomb(self, tomb):
+        self.tomb.append(tomb)
