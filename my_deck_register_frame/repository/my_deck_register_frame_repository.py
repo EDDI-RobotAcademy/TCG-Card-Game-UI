fa177fc0d7a19f0ce19e069780a2bd2bdfeed6ea
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2024-01-26 15:54:18 +0900

    feat:[이재림]덱 생성 기능[TCG-CGU-104]

---
+++ my_deck_register_frame/repository/my_deck_register_frame_repository.py
@@ -0,0 +1,11 @@
+import abc
+
+class MyDeckRegisterFrameRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def saveTransmitIpcChannel(self, transmitIpcChannel):
+        pass
+
+    @abc.abstractmethod
+    def saveReceiveIpcChannel(self, receiveIpcChannel):
+        pass
