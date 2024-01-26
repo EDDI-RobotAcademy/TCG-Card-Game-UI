1a0212cd8603e5adbb73291a28fa055e0ef16938
Jungpop <shockstrut22@gmail.com>
2024-01-26 10:20:13 +0900

    게임플레이어가 in game에서 battle field의 로스트 존 UI를 볼 수 있습니다. [TCG-CGU-110]

---
+++ opengl_trap/trap.py
@@ -0,0 +1,38 @@
+from opengl_shape.image_element import ImageElement
+from opengl_shape.rectangle import Rectangle
+
+
+class Trap:
+    __imagePath = None
+
+    def __init__(self, local_translation=(0, 0), scale=1):
+        self.shapes = []
+        self.local_translation = local_translation
+        self.scale = scale
+
+    def get_trap_shapes(self):
+        return self.shapes
+    def change_local_translation(self, _translation):
+        self.local_translation = _translation
+
+    def add_shape(self, shape):
+        shape.local_translate(self.local_translation)
+        self.shapes.append(shape)
+    def create_trap_rectangle(self, color, vertices):
+        tomb_base = Rectangle(color=color,
+                                       vertices=vertices)
+        tomb_base.set_visible(True)
+        self.add_shape(tomb_base)
+
+    def create_illustration(self, image_path, vertices):
+        unit_illustration = ImageElement(image_path=image_path,
+                                         vertices=vertices)
+        self.add_shape(unit_illustration)
+
+    def init_shapes(self, image_path):
+        self.__imagePath = image_path
+        self.create_trap_rectangle(color=(0, 0, 0, 1.0),
+                                   vertices=[(1400, 200), (1600, 200), (1600, 400), (1400, 400)])
+
+        self.create_illustration(image_path=self.__imagePath,
+                                 vertices=[(1400, 200), (1600, 200), (1600, 400), (1400, 400)])
