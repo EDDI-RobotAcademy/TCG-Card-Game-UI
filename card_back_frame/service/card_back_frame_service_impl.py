be89417f78a42002d18b320d38da40e4bb326f26
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2024-01-28 19:27:15 +0900

    remove:[조승훈]카드 백프레임에서 사용하던 tk.inter형 rendering방식 제거[TCG-CGU-49]

--- card_back_frame/service/card_back_frame_service_impl.py
+++ card_back_frame/service/card_back_frame_service_impl.py
@@ -1,9 +1,4 @@
-from card.card_left_bottom_rendering.service.card_left_bottom_rendering_service_impl import \
-    CardLeftBottomRenderingServiceImpl
-from card.card_left_up_rendering.service.card_left_up_rendering_service_impl import CardLeftUpRenderingServiceImpl
-from card.card_right_bottom_rendering.service.card_right_bottom_rendering_service_impl import \
-    CardRightBottomRenderingServiceImpl
-from card.card_right_up_rendering.service.card_right_up_rendering_service_impl import CardRightUpRenderingServiceImpl
+
 from card_back_frame.repository.card_back_frame_repository_impl import CardBackFrameRepositoryImpl
 from card_back_frame.service.card_back_frame_service import CardBackFrameService
 class CardBackFrameServiceImpl(CardBackFrameService):
@@ -13,10 +8,6 @@ class CardBackFrameServiceImpl(CardBackFrameService):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
             cls.__instance.__cardBackFrameRepository = CardBackFrameRepositoryImpl.getInstance()
-            cls.__instance.__cardLeftBottomRenderingService = CardLeftBottomRenderingServiceImpl.getInstance()
-            cls.__instance.__cardLeftUpRenderingService = CardLeftUpRenderingServiceImpl.getInstance()
-            cls.__instance.__cardRightBottomRenderingService = CardRightBottomRenderingServiceImpl.getInstance()
-            cls.__instance.__cardRightUpRenderingService = CardRightUpRenderingServiceImpl.getInstance()
         return cls.__instance
 
     @classmethod
@@ -27,15 +18,5 @@ class CardBackFrameServiceImpl(CardBackFrameService):
 
     def createCardBackUiFrame(self, rootWindow):
         CardBackFrame = self.__cardBackFrameRepository.createCardBackFrame(rootWindow)
-        CardLeftBottom = self.__cardLeftBottomRenderingService.createCardLeftBottomRenderingUiFrame(CardBackFrame)
-        CardLeftBottom.place(relx=0, rely=0.76)
 
-        CardLeftUp = self.__cardLeftUpRenderingService.createCardLeftUpRenderingUiFrame(CardBackFrame)
-        CardLeftUp.place(relx=0, rely=0)
-
-        CardRightBottom = self.__cardRightBottomRenderingService.createCardRightBottomRenderingUiFrame(CardBackFrame)
-        CardRightBottom.place(relx=0.60, rely=0)
-
-        CardRightUp = self.__cardRightUpRenderingService.createCardRightUpRenderingUiFrame(CardBackFrame)
-        CardRightUp.place(relx=0.6, rely=0.76)
         return CardBackFrame
