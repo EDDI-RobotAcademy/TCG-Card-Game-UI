b09c04ae73ec88220d597b3990bd0fb97a03556f
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2024-01-30 18:26:04 +0900

    chore: [이재림] 나의 덱 생성 관련 불필요한 코드 삭제 [TCG-CGU-44]

--- opengl_my_deck_register_frame/entity/my_deck_register_scene.py
+++
@@ -1,8 +0,0 @@
-class MyDeckRegisterScene:
-    def __init__(self):
-        self.shapes = []
-        self.translations = []
-
-    def add_shape(self, shape, translation=(0, 0)):
-        self.shapes.append(shape)
-        self.translations.append(translation)
