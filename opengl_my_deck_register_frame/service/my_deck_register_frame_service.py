f5fbe49bb4ca95f6aba03b91eb59e1b3b42fd99e
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2024-01-31 16:10:01 +0900

    update: [이재림] 생성할 덱 이름 입력 후 server로 전송하여 덱 생성 요청 가능 [TCG-CGU-104]

--- opengl_my_deck_register_frame/service/my_deck_register_frame_service.py
+++ opengl_my_deck_register_frame/service/my_deck_register_frame_service.py
@@ -2,8 +2,12 @@ import abc
 
 
 class MyDeckRegisterFrameService(abc.ABC):
+    # @abc.abstractmethod
+    # def createMyDeckRegisterUiFrame(self, rootWindow, switchFrameWithMenuName):
+    #     pass
+
     @abc.abstractmethod
-    def createMyDeckRegisterUiFrame(self, rootWindow, switchFrameWithMenuName):
+    def on_deck_register_click(self, entry_deckName):
         pass
 
     @abc.abstractmethod
