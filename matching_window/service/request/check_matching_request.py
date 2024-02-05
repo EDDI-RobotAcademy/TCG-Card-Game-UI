1d3ee6832cf6482b8ec5ca0f50bda6523d5dc41a
LoHenP <qwpoal119@naver.com>
2024-02-05 10:26:22 +0900

    rename: [이재승] 프로토콜 이름 변경

--- matching_window/service/request/check_matching_request.py
+++ matching_window/service/request/check_matching_request.py
@@ -3,7 +3,7 @@ from common.protocol import CustomProtocol
 
 class CheckMatchingRequest:
     def __init__(self, sessionInfo):
-        self.__protocolNumber = CustomProtocol.CHECK_MATCHING.value
+        self.__protocolNumber = CustomProtocol.BATTLE_READY.value
         self.__sessionInfo = sessionInfo
 
 
