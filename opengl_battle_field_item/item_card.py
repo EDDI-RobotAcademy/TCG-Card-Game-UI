b9f2395b5e2213f4101c035c0ecff9771a61c4fe
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2024-02-14 16:08:44 +0900

    update:[조승훈]원에 일러스트 렌더링을 할 때 태두리가 안따라오는 문제가 발생합니다[TCG-CGU-148]

--- opengl_battle_field_item/item_card.py
+++ opengl_battle_field_item/item_card.py
@@ -1,6 +1,10 @@
 import os
 
+from card_info_from_csv.repository.card_info_from_csv_repository_impl import CardInfoFromCsvRepositoryImpl
+from common.utility import get_project_root
 from opengl_shape.circle import Circle
+from opengl_shape.image_circle_element import ImageCircleElement
+
 
 class ItemCard:
     __imagePath = None
@@ -8,6 +12,7 @@ class ItemCard:
     def __init__(self, local_translation=(0, 0)):
         self.shapes = []
         self.local_translation = local_translation
+        self.cardInfoFromCsvRepositoryImpl = CardInfoFromCsvRepositoryImpl()
 
     def change_local_translation(self, _translation):
         self.local_translation = _translation
@@ -20,34 +25,38 @@ class ItemCard:
         self.shapes.append(shape)
 
     def create_item_energy_circle(self, color, center, radius):
-        unit_energy_circle = Circle(color=color,
+        item_energy_circle = Circle(color=color,
                                     center=center,
                                     radius=radius)
-        self.add_shape(unit_energy_circle)
+        self.add_shape(item_energy_circle)
 
-    def create_item_race_circle(self, color, center, radius):
-        unit_tribe_circle = Circle(color=color,
-                                   center=center,
-                                   radius=radius)
-        self.add_shape(unit_tribe_circle)
+    def create_item_race_illustration_circle(self, image_path, center, radius):
+        item_race_circle = ImageCircleElement(image_path=image_path,
+                                              center=center,
+                                              radius=radius)
+        self.add_shape(item_race_circle)
 
-    def create_item_type_circle(self, color, center, radius):
-        unit_attack_circle = Circle(color=color,
-                                    center=center,
-                                    radius=radius)
-        self.add_shape(unit_attack_circle)
+    def create_item_type_illustration_circle(self, image_path, center, radius):
+        item_type_circle = ImageCircleElement(image_path=image_path,
+                                              center=center,
+                                              radius=radius)
+        self.add_shape(item_type_circle)
 
 
     def init_shapes(self, circle_radius, card_number, rectangle_height, rectangle_width):
+
+        project_root = get_project_root()
 
         self.create_item_energy_circle(color=(1.0, 0.33, 0.34, 1.0),
                                        center=(0, 0),
                                        radius=circle_radius)
 
-        self.create_item_race_circle(color=(0.678, 0.847, 0.902, 1.0),
-                                     center=(rectangle_width, 0),
-                                     radius=circle_radius)
+        self.create_item_race_illustration_circle(
+            image_path=os.path.join(project_root, "local_storage", "card_race_image", f"{self.cardInfoFromCsvRepositoryImpl.getCardRaceForCardNumber(card_number)}.png"),
+            center=(rectangle_width, 0),
+            radius=circle_radius)
 
-        self.create_item_type_circle(color=(0.988, 0.976, 0.800, 1.0),
-                                     center=(rectangle_width, rectangle_height),
-                                     radius=circle_radius)
+        self.create_item_type_illustration_circle(
+            image_path=os.path.join(project_root, "local_storage", "card_type_image", f"{self.cardInfoFromCsvRepositoryImpl.getCardTypeDictionary(card_number)}.png"),
+            center=(rectangle_width, rectangle_height),
+            radius=circle_radius)
