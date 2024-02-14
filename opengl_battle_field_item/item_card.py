81e3bf9a4433cbde15f2470d4781e0e4d595e818
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2024-02-14 12:19:18 +0900

    feat:[조승훈]다른 장소에서 Card를 사용하기 편하도록 카드의 크기 조정 가능합니다.[TCG-CGU-194]

--- opengl_battle_field_item/item_card.py
+++ opengl_battle_field_item/item_card.py
@@ -5,10 +5,9 @@ from opengl_shape.circle import Circle
 class ItemCard:
     __imagePath = None
 
-    def __init__(self, local_translation=(0, 0), scale=1):
+    def __init__(self, local_translation=(0, 0)):
         self.shapes = []
         self.local_translation = local_translation
-        self.scale = scale
 
     def change_local_translation(self, _translation):
         self.local_translation = _translation
@@ -39,16 +38,16 @@ class ItemCard:
         self.add_shape(unit_attack_circle)
 
 
-    def init_shapes(self, circle_radius, card_number):
+    def init_shapes(self, circle_radius, card_number, rectangle_height, rectangle_width):
 
         self.create_item_energy_circle(color=(1.0, 0.33, 0.34, 1.0),
                                        center=(0, 0),
                                        radius=circle_radius)
 
         self.create_item_race_circle(color=(0.678, 0.847, 0.902, 1.0),
-                                     center=(124, 0),
+                                     center=(rectangle_width, 0),
                                      radius=circle_radius)
 
         self.create_item_type_circle(color=(0.988, 0.976, 0.800, 1.0),
-                                     center=(124, 200),
+                                     center=(rectangle_width, rectangle_height),
                                      radius=circle_radius)
