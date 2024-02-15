356b85d56d816b7de7ee8030dd773ea2cd3f79d8
silenc3502 <gcccompil3r@gmail.com>
2024-02-16 02:48:22 +0900

    feat: [이상훈] 배틀 필드 상에서 당신의 Hand를 표현합니다 [TCG-CGU-228]

--- tests/lsh/ugly_draw_battle_field/test_draw_battle_field_refactor.py
+++ tests/lsh/ugly_draw_battle_field/test_draw_battle_field_refactor.py
@@ -8,6 +8,7 @@ from battle_field.components.opponent_tomb import OpponentTomb
 from battle_field.components.opponent_trap import OpponentTrap
 from battle_field.components.opponent_unit_field import OpponentUnitField
 from battle_field.components.your_deck import YourDeck
+from battle_field.components.your_hand_panel import YourHandPanel
 from battle_field.components.your_lost_zone import YourLostZone
 from battle_field.components.your_main_character import YourMainCharacter
 from battle_field.components.your_tomb import YourTomb
@@ -27,42 +28,6 @@ from pyopengltk import OpenGLFrame
 
 from pre_drawed_image_manager.pre_drawed_image import PreDrawedImage
 from opengl_shape.rectangle import Rectangle
-
-
-class YourHandPanel:
-    __pre_drawed_image_instance = PreDrawedImage.getInstance()
-
-    def __init__(self, local_translation=(0, 0), scale=1):
-        self.pre_drawed_your_hand = None
-        self.shapes = []
-        self.local_translation = local_translation
-        self.scale = scale
-
-        self.pickable_card_list = []
-
-    def change_local_translation(self, _translation):
-        self.local_translation = _translation
-
-    def get_your_hand_panel_shapes(self):
-        return self.shapes
-
-    def get_pickable_card_list(self):
-        return self.pickable_card_list
-
-    def add_shape(self, shape):
-        shape.local_translate(self.local_translation)
-        self.shapes.append(shape)
-
-    def create_hand_panel(self, image_data, vertices):
-        hand_panel = RectangleImage(image_data=image_data,
-                                    vertices=vertices)
-        self.add_shape(hand_panel)
-
-    def init_shapes(self):
-        self.__pre_drawed_image_instance.pre_draw_your_hand_panel()
-
-        self.create_hand_panel(image_data=self.__pre_drawed_image_instance.get_pre_draw_your_hand_panel(),
-                               vertices=[(300, 830), (1600, 830), (1600, 980), (300, 980)])
 
 
 class YourUnitField:
