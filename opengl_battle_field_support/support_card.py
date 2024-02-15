56105f17924d879e18857b7bd03e608f00a901d0
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2024-02-15 15:50:12 +0900

    feat:[조승훈]카드의 종류별로 특성을 지닌 일러스트를 붙여 그릴 수 있습니다.[TCG-CGU-208]

--- opengl_battle_field_support/support_card.py
+++ opengl_battle_field_support/support_card.py
@@ -1,6 +1,9 @@
 import os
 
-from opengl_shape.circle import Circle
+from card_info_from_csv.repository.card_info_from_csv_repository_impl import CardInfoFromCsvRepositoryImpl
+from common.utility import get_project_root
+from opengl_shape.image_circle_element import ImageCircleElement
+
 
 class SupportCard():
     __imagePath = None
@@ -8,6 +11,7 @@ class SupportCard():
     def __init__(self, local_translation=(0, 0)):
         self.shapes = []
         self.local_translation = local_translation
+        self.cardInfoFromCsvRepositoryImpl = CardInfoFromCsvRepositoryImpl()
 
     def change_local_translation(self, _translation):
         self.local_translation = _translation
@@ -19,25 +23,30 @@ class SupportCard():
         shape.local_translate(self.local_translation)
         self.shapes.append(shape)
 
-    def create_support_race_circle(self, color, center, radius):
-        unit_tribe_circle = Circle(color=color,
-                                   center=center,
-                                   radius=radius)
-        self.add_shape(unit_tribe_circle)
+    def create_support_race_illustration_circle(self, image_path, center, radius):
+        support_race_circle = ImageCircleElement(image_path=image_path,
+                                                 center=center,
+                                                 radius=radius)
+        self.add_shape(support_race_circle)
 
-    def create_support_type_circle(self, color, center, radius):
-        unit_attack_circle = Circle(color=color,
-                                    center=center,
-                                    radius=radius)
-        self.add_shape(unit_attack_circle)
+    def create_support_type_illustration_circle(self, image_path, center, radius):
+        support_type_circle = ImageCircleElement(image_path=image_path,
+                                                 center=center,
+                                                 radius=radius)
+        self.add_shape(support_type_circle)
 
 
     def init_shapes(self, circle_radius, card_number, rectangle_height, rectangle_width):
+        project_root = get_project_root()
 
-        self.create_support_race_circle(color=(0.678, 0.847, 0.902, 1.0),
-                                        center=(rectangle_width, 0),
-                                        radius=circle_radius)
+        self.create_support_race_illustration_circle(
+            image_path=os.path.join(project_root, "local_storage", "card_race_image",
+                                    f"{self.cardInfoFromCsvRepositoryImpl.getCardRaceForCardNumber(card_number)}.png"),
+            center=(rectangle_width, 0),
+            radius=circle_radius)
 
-        self.create_support_type_circle(color=(0.988, 0.976, 0.800, 1.0),
-                                        center=(rectangle_width, rectangle_height),
-                                        radius=circle_radius)
+        self.create_support_type_illustration_circle(
+            image_path=os.path.join(project_root, "local_storage", "card_type_image",
+                                    f"{self.cardInfoFromCsvRepositoryImpl.getCardTypeForCardNumber(card_number)}.png"),
+            center=(rectangle_width, rectangle_height),
+            radius=circle_radius)
