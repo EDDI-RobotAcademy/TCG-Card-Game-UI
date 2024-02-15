a754392c92f6b08fa0507e64db20bdf25901512e
silenc3502 <gcccompil3r@gmail.com>
2024-02-16 02:45:47 +0900

    feat: [이상훈] 배틀 필드 상에서 당신의 Main Character를 표현합니다 [TCG-CGU-227]

--- tests/lsh/ugly_draw_battle_field/test_draw_battle_field_refactor.py
+++ tests/lsh/ugly_draw_battle_field/test_draw_battle_field_refactor.py
@@ -9,6 +9,7 @@ from battle_field.components.opponent_trap import OpponentTrap
 from battle_field.components.opponent_unit_field import OpponentUnitField
 from battle_field.components.your_deck import YourDeck
 from battle_field.components.your_lost_zone import YourLostZone
+from battle_field.components.your_main_character import YourMainCharacter
 from battle_field.components.your_tomb import YourTomb
 from battle_field.components.your_trap import YourTrap
 from image_shape.oval_image import OvalImage
@@ -26,45 +27,6 @@ from pyopengltk import OpenGLFrame
 
 from pre_drawed_image_manager.pre_drawed_image import PreDrawedImage
 from opengl_shape.rectangle import Rectangle
-
-
-class YourMainCharacter:
-    __pre_drawed_image_instance = PreDrawedImage.getInstance()
-
-    def __init__(self, local_translation=(0, 0), scale=1):
-        self.pre_drawed_main_character = None
-        self.shapes = []
-        self.local_translation = local_translation
-        self.scale = scale
-
-    def change_main_character_translation(self, _translation):
-        self.local_translation = _translation
-
-    def get_main_character_shapes(self):
-        return self.shapes
-
-    def add_shape(self, shape):
-        shape.local_translate(self.local_translation)
-        self.shapes.append(shape)
-
-    def create_main_character(self, image_data, center, radius_x, radius_y):
-        main_character_illustration = OvalImage(image_data=image_data,
-                                                center=center,
-                                                radius_x=radius_x,
-                                                radius_y=radius_y)
-        print(f"main_character_illustration{main_character_illustration}")
-        print(type(main_character_illustration))
-        self.add_shape(main_character_illustration)
-
-    def init_your_main_character_shapes(self):
-        radius_y = 40
-        radius_x = radius_y * ((1 + math.sqrt(5)) / 2)
-        self.__pre_drawed_image_instance.pre_draw_your_main_character()
-
-        self.create_main_character(image_data=self.__pre_drawed_image_instance.get_pre_draw_your_main_character(),
-                                   center=(960, 1034),
-                                   radius_x=radius_x,
-                                   radius_y=radius_y)
 
 
 class YourHandPanel:
