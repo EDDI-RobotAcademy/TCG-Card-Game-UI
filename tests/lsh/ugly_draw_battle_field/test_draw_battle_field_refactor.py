de18c7856efa7637a64c6521727040db1cee0f56
silenc3502 <gcccompil3r@gmail.com>
2024-02-16 02:41:01 +0900

    feat: [이상훈] 배틀 필드 상에서 당신의 Trap을 표현합니다 [TCG-CGU-225]

--- tests/lsh/ugly_draw_battle_field/test_draw_battle_field_refactor.py
+++ tests/lsh/ugly_draw_battle_field/test_draw_battle_field_refactor.py
@@ -9,6 +9,7 @@ from battle_field.components.opponent_trap import OpponentTrap
 from battle_field.components.opponent_unit_field import OpponentUnitField
 from battle_field.components.your_lost_zone import YourLostZone
 from battle_field.components.your_tomb import YourTomb
+from battle_field.components.your_trap import YourTrap
 from image_shape.oval_image import OvalImage
 from image_shape.rectangle_image import RectangleImage
 from opengl_shape.oval import Oval
@@ -24,37 +25,6 @@ from pyopengltk import OpenGLFrame
 
 from pre_drawed_image_manager.pre_drawed_image import PreDrawedImage
 from opengl_shape.rectangle import Rectangle
-
-
-class YourTrap:
-    __pre_drawed_image_instance = PreDrawedImage.getInstance()
-
-    def __init__(self, local_translation=(0, 0), scale=1):
-        self.pre_drawed_trap = None
-        self.shapes = []
-        self.local_translation = local_translation
-        self.scale = scale
-
-    def get_trap_shapes(self):
-        return self.shapes
-
-    def change_local_translation(self, _translation):
-        self.local_translation = _translation
-
-    def add_shape(self, shape):
-        shape.local_translate(self.local_translation)
-        self.shapes.append(shape)
-
-    def create_trap(self, image_data, vertices):
-        trap_illustration = RectangleImage(image_data=image_data,
-                                           vertices=vertices)
-        self.add_shape(trap_illustration)
-
-    def init_shapes(self):
-        self.__pre_drawed_image_instance.pre_draw_your_trap()
-        # -1040, 480
-        self.create_trap(image_data=self.__pre_drawed_image_instance.get_pre_draw_your_trap(),
-                         vertices=[(300, 680), (500, 680), (500, 880), (300, 880)])
 
 
 class YourCardDeck:
