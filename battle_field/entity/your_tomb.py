84a9dfdb0a5f21ad52bb4c3ea7e05c91309ef6b1
Jungpop <shockstrut22@gmail.com>
2024-02-22 17:38:34 +0900

    fix: [조정환] 플레이어가 사용한 카드를 Tomb에서 확인할 수 있습니다. -> fix가 필요합니다.,[TCG-CGU-306]

--- battle_field/entity/your_tomb.py
+++ battle_field/entity/your_tomb.py
@@ -1,5 +1,7 @@
 import os
 
+from battle_field.infra.your_tomb_repository import YourTombRepository
+from battle_field.state.current_tomb import CurrentTombState
 from battle_field_function.controller.battle_field_function_controller_impl import BattleFieldFunctionControllerImpl
 from battle_field_ui_button.battle_field_button import BattleFieldButton
 from pre_drawed_image_manager.pre_drawed_image import PreDrawedImage
@@ -14,6 +16,8 @@ class YourTomb:
         self.shapes = []
         self.local_translation = local_translation
         self.scale = scale
+        self.your_tomb_repository = YourTombRepository()
+        self.current_tomb_state = CurrentTombState()
 
     def get_tomb_shapes(self):
         return self.shapes
@@ -41,4 +45,11 @@ class YourTomb:
 
     def invoke_click_event(self):
         print("your_tomb_button.invoke_click_event!!!!")
-        # self.__battle_field_function_controller.callTurnEnd()
+        print(f"self.your_tomb_repository.get_current_tomb_state(): {self.your_tomb_repository.get_current_tomb_state()}")
+        for tomb_card in self.your_tomb_repository.get_current_tomb_state():
+            print(f"tomb_card repo: {tomb_card}")
+            tomb_card = self.your_tomb_repository.create_tomb_card(tomb_card)
+            print(f"self.your_tomb_repository.current_tomb_unit_list: {self.your_tomb_repository.get_current_tomb_unit_list()}")
+            print(f"tomb_card repo: {tomb_card}")
+            tomb_card.draw()
+
