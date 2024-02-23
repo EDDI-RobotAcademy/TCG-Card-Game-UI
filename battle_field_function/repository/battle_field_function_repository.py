593e8e7879e0bfec207e848cde4a269d7f48a430
LoHenP <qwpoal119@naver.com>
2024-02-23 11:53:20 +0900

    delete: [이재승] 안쓰이는 기능들 삭제

--- battle_field_function/repository/battle_field_function_repository.py
+++ battle_field_function/repository/battle_field_function_repository.py
@@ -27,43 +27,5 @@ class BattleFieldFunctionRepository(abc.ABC):
     def requestTurnEnd(self, turnEndRequest):
         pass
 
-    @abc.abstractmethod
-    def requestUseUnitCard(self, useUnitCardRequest):
-        pass
-
-    @abc.abstractmethod
-    def requestUnitAttack(self, unitAttackRequest):
-        pass
-
-    @abc.abstractmethod
-    def requestUseEnergyCard(self, useEnergyCardRequest):
-        pass
-
-    @abc.abstractmethod
-    def requestUseSpecialEnergyCard(self, useSpecialEnergyCardRequest):
-        pass
-
-    @abc.abstractmethod
-    def requestUseEnvironmentCard(self, useEnvironmentCardRequest):
-        pass
-
-    @abc.abstractmethod
-    def requestUseItemCard(self, useItemCardRequest):
-        pass
-
-
-    @abc.abstractmethod
-    def requestUseSupportCard(self, useSupportCardRequest):
-        pass
-
-    @abc.abstractmethod
-    def requestUseToolCard(self, useToolCardRequest):
-        pass
-
-    @abc.abstractmethod
-    def requestUseTrapCard(self, useTrapCardRequest):
-        pass
-
-
 
 
