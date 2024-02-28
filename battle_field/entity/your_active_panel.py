f734934e3d4b6663cb7853b8d7fb74622df2c930
Sanghoon Lee <gcccompil3r@gmail.com>
2024-02-28 11:41:11 +0900

    feat: [이상훈] Active Panel 스킬 버튼 생성 및 클릭 감지 [TCG-CGU-386]

--- battle_field/entity/your_active_panel.py
+++ battle_field/entity/your_active_panel.py
@@ -10,6 +10,10 @@ class YourActivePanel:
 
     def __init__(self):
         self.your_active_panel = None
+        self.your_active_panel_attack_button = None
+        self.your_active_panel_first_skill_button = None
+        self.your_active_panel_second_skill_button = None
+        self.your_active_panel_third_skill_button = None
 
         self.total_width = None
         self.total_height = None
@@ -40,13 +44,29 @@ class YourActivePanel:
         return self.your_active_panel
 
     def clear_your_active_panel(self):
+        del self.your_active_panel
         self.your_active_panel = None
 
     def get_your_active_panel_attack_button(self):
         return self.your_active_panel_attack_button
 
     def clear_your_active_panel_attack_button(self):
+        del self.your_active_panel_attack_button
         self.your_active_panel_attack_button = None
+
+    def get_your_active_panel_first_skill_button(self):
+        return self.your_active_panel_first_skill_button
+
+    def clear_your_active_panel_first_skill_button(self):
+        del self.your_active_panel_first_skill_button
+        self.your_active_panel_first_skill_button = None
+
+    def get_your_active_panel_second_skill_button(self):
+        return self.your_active_panel_second_skill_button
+
+    def clear_your_active_panel_second_skill_button(self):
+        del self.your_active_panel_second_skill_button
+        self.your_active_panel_second_skill_button = None
 
     def create_general_attack_button(self, image_data, vertices):
         general_attack_button = RectangleImage(image_data=image_data,
@@ -55,11 +75,26 @@ class YourActivePanel:
         general_attack_button.set_initial_vertices(vertices)
         return general_attack_button
 
+    def create_first_skill_button(self, image_data, vertices):
+        first_skill_button = RectangleImage(image_data=image_data,
+                                            vertices=vertices)
+
+        first_skill_button.set_initial_vertices(vertices)
+        return first_skill_button
+
+    def create_second_skill_button(self, image_data, vertices):
+        second_skill_button = RectangleImage(image_data=image_data,
+                                             vertices=vertices)
+
+        second_skill_button.set_initial_vertices(vertices)
+        return second_skill_button
+
     def create_your_active_panel(self, start_point, selected_object):
         card_id = selected_object.get_card_number()
         skill_count = self.card_info_repository.getCardSkillCounterForCardNumber(card_id)
 
         width_size = 120 * self.width_ratio
+        base_height_size = 74.2 * self.height_ratio
         height_size = (74.2 * self.height_ratio) * (skill_count + 1)
 
         rectangle_color = (1.0, 1.0, 1.0, 0.6)
@@ -77,27 +112,70 @@ class YourActivePanel:
             (0, 0),
             (0, 0))
 
+        print(f"active panel -> start_point: {start_point}, end_point: {end_point}")
+
         button_width_size = 100 * self.width_ratio
         button_height_size = 62 * self.height_ratio
+
+        height_margin = (base_height_size - button_height_size)
 
         button_end_point = (start_point[0] + button_width_size, start_point[1] + button_height_size)
 
         self.your_active_panel_attack_button = self.create_general_attack_button(
             image_data=self.pre_drawed_image_instance.get_pre_draw_ok_button(),
             vertices=[
-                (start_point[0]      + 10, start_point[1] + 10),
-                (button_end_point[0] + 10, start_point[1] + 10),
+                (start_point[0]      + 10, start_point[1] + height_margin),
+                (button_end_point[0] + 10, start_point[1] + height_margin),
                 (button_end_point[0] + 10, button_end_point[1]),
                 (start_point[0]      + 10, button_end_point[1])])
+
+        print(f"attack button -> start_point: {start_point}, button_end_point: {button_end_point}")
+
+        if skill_count == 0:
+            return
+
+        first_skill_start_point = (start_point[0],                   start_point[1] + base_height_size)
+        first_skill_end_point = (start_point[0] + button_width_size, start_point[1] + base_height_size + button_height_size)
+
+        print(f"skill button -> skill_start_point: {first_skill_start_point}, button_end_point: {first_skill_end_point}")
+
+        self.your_active_panel_first_skill_button = self.create_first_skill_button(
+            image_data=self.pre_drawed_image_instance.get_pre_draw_ok_button(),
+            vertices=[
+                (first_skill_start_point[0] + 10, first_skill_start_point[1] + 10),
+                (first_skill_end_point[0] + 10, first_skill_start_point[1] + 10),
+                (first_skill_end_point[0] + 10, first_skill_end_point[1]),
+                (first_skill_start_point[0] + 10, first_skill_end_point[1])])
+
+        if skill_count == 1:
+            return
+
+        second_skill_start_point = (start_point[0], start_point[1] + base_height_size * 2)
+        second_skill_end_point = (start_point[0] + button_width_size, start_point[1] + base_height_size * 2 + button_height_size)
+
+        print(f"skill button -> skill_start_point: {second_skill_start_point}, button_end_point: {second_skill_end_point}")
+
+        self.your_active_panel_second_skill_button = self.create_second_skill_button(
+            image_data=self.pre_drawed_image_instance.get_pre_draw_ok_button(),
+            vertices=[
+                (second_skill_start_point[0] + 10, second_skill_start_point[1] + 10),
+                (second_skill_end_point[0] + 10, second_skill_start_point[1] + 10),
+                (second_skill_end_point[0] + 10, second_skill_end_point[1]),
+                (second_skill_start_point[0] + 10, second_skill_end_point[1])])
+
+        if skill_count == 2:
+            return
+
+        self.your_active_panel_third_skill_button = None
 
     def is_point_inside_attack_button(self, point):
         point_x, point_y = point
         point_y *= -1
 
-        print(f"point_x: {point_x}, point_y: {point_y}")
+        # print(f"point_x: {point_x}, point_y: {point_y}")
 
         attack_button = self.get_your_active_panel_attack_button()
-        print(f"attack_button vertices: {attack_button.get_vertices()}")
+        # print(f"attack_button vertices: {attack_button.get_vertices()}")
 
         translated_vertices = [
             (x * self.width_ratio + attack_button.local_translation[0] * self.width_ratio, y * self.height_ratio + attack_button.local_translation[1] * self.height_ratio)
@@ -111,3 +189,60 @@ class YourActivePanel:
 
         print("your attack_button -> True")
         return True
+
+    def is_point_inside_first_skill_button(self, point):
+        point_x, point_y = point
+        point_y *= -1
+
+        first_skill_button = self.get_your_active_panel_first_skill_button()
+
+        translated_vertices = [
+            (x * self.width_ratio + first_skill_button.local_translation[0] * self.width_ratio, y * self.height_ratio + first_skill_button.local_translation[1] * self.height_ratio)
+            for x, y in first_skill_button.get_vertices()
+        ]
+
+        if not (translated_vertices[0][0] <= point_x <= translated_vertices[2][0] and
+                translated_vertices[1][1] <= point_y <= translated_vertices[2][1]):
+            print("your attack_button result -> False")
+            return False
+
+        print("your attack_button -> True")
+        return True
+
+    def is_point_inside_second_skill_button(self, point):
+        point_x, point_y = point
+        point_y *= -1
+
+        second_skill_button = self.get_your_active_panel_second_skill_button()
+
+        translated_vertices = [
+            (x * self.width_ratio + second_skill_button.local_translation[0] * self.width_ratio, y * self.height_ratio + second_skill_button.local_translation[1] * self.height_ratio)
+            for x, y in second_skill_button.get_vertices()
+        ]
+
+        if not (translated_vertices[0][0] <= point_x <= translated_vertices[2][0] and
+                translated_vertices[1][1] <= point_y <= translated_vertices[2][1]):
+            print("your attack_button result -> False")
+            return False
+
+        print("your attack_button -> True")
+        return True
+
+    # def is_point_inside_third_skill_button(self, point):
+    #     point_x, point_y = point
+    #     point_y *= -1
+    #
+    #     third_skill_button = self.get_your_active_panel_third_skill_button()
+    #
+    #     translated_vertices = [
+    #         (x * self.width_ratio + second_skill_button.local_translation[0] * self.width_ratio, y * self.height_ratio + second_skill_button.local_translation[1] * self.height_ratio)
+    #         for x, y in second_skill_button.get_vertices()
+    #     ]
+    #
+    #     if not (translated_vertices[0][0] <= point_x <= translated_vertices[2][0] and
+    #             translated_vertices[1][1] <= point_y <= translated_vertices[2][1]):
+    #         print("your attack_button result -> False")
+    #         return False
+    #
+    #     print("your attack_button -> True")
+    #     return True
