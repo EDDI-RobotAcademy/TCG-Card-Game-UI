f29c28dc77edce5d773d5ab49203cffcee8c7664
LoHenP <44047542+LoHenP@users.noreply.github.com>
2024-03-02 15:24:01 +0900

    Update left_click_detector.py

--- battle_field/components/mouse_left_click/left_click_detector.py
+++ battle_field/components/mouse_left_click/left_click_detector.py
@@ -178,3 +178,24 @@ class LeftClickDetector:
             return turn_end_button
 
         return None
+
+    def which_one_select_is_in_close_option_area(self, click_point, option, canvas_height):
+        x, y = click_point
+        y = canvas_height - y
+        y *= -1
+
+        if option.is_point_inside_close((x, y)):
+            return option
+
+        return None
+
+
+    def which_one_select_is_in_close_surrender_confirm_area(self, click_point, surrender_confirm, canvas_height):
+        x, y = click_point
+        y = canvas_height - y
+        y *= -1
+
+        if surrender_confirm.is_point_inside_close((x, y)):
+            return surrender_confirm
+
+        return None
