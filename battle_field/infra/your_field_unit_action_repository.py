f9cf66235dec1e095f901321801b004aaa9478d1
Sanghoon Lee <gcccompil3r@gmail.com>
2024-03-04 09:46:47 +0900

    feat: [이상훈] 필드 출격 유닛 Action Count 관리 [TCG-CGU-421]

---
+++ battle_field/infra/your_field_unit_action_repository.py
@@ -0,0 +1,29 @@
+from battle_field.state.current_field_unit_action_count import CurrentFieldUnitActionCountState
+
+
+class CurrentFieldUnitActionRepository:
+    __instance = None
+
+    current_field_unit_action_count = CurrentFieldUnitActionCountState()
+    # current_field_unit_action_status = CurrentFieldUnitActionStatus()
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+        return cls.__instance
+
+    def create_field_unit_action_count(self, count):
+        self.current_field_unit_action_count.make_field_unit_action_count(count)
+        print(f"create_field_unit_action_count_by_index() -> count {count}")
+
+    def get_current_field_unit_action_count(self):
+        return self.current_field_unit_action_count.get_your_field_unit_list_action_count()
+
+
+
