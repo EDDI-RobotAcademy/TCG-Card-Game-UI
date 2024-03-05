0af83be01ff2a1eaa6ff18ede86bca0f7aee97bd
tkdrms420 <149447077+tkdrms420@users.noreply.github.com>
2024-03-05 15:36:07 +0900

    test: [윤상근] test_notify_function

---
+++ tests/ysg/test_notify_function/test_notify_reader/controller/notify_reader_controller.py
@@ -0,0 +1,15 @@
+import abc
+
+
+class NotifyReaderController(abc.ABC):
+    @abc.abstractmethod
+    def requestToReadNotifyCommand(self):
+        pass
+
+    @abc.abstractmethod
+    def requestToInjectNoWaitIpcChannel(self, no_wait_ipc_channel):
+        pass
+
+    @abc.abstractmethod
+    def requestToMappingNoticeWithFunction(self):
+        pass
