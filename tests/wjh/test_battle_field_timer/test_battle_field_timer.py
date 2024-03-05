3d849f1aac5002f39de45d5aa340343072d01178
Amicitia00 <wjh324@naver.com>
2024-03-05 17:51:59 +0900

    feat:[우정훈]test_battle_field_timer부분 수정중

--- tests/wjh/test_battle_field_timer/test_battle_field_timer.py
+++ tests/wjh/test_battle_field_timer/test_battle_field_timer.py
@@ -1,31 +1,64 @@
-import tkinter
+import tkinter as tk
 import unittest
-from PIL import ImageTk, Image
 
-from rock_paper_scissors.repository.rock_paper_scissors_repository_impl import RockPaperScissorsRepositoryImpl
-from rock_paper_scissors.service.request.rock_paper_scissors_request import RockPaperScissorsRequest
-from session.repository.session_repository_impl import SessionRepositoryImpl
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from pyopengltk import OpenGLFrame
+
+class battleFieldTimer(OpenGLFrame):
+    def __init__(self):
+        super().__init__()
+        self.timer = 0
+
+    def initgl(self):
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        gluOrtho2D(-1.0, 1.0, -1.0, 1.0)
+
+    def redraw(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        glColor3f(1.0, 1.0, 1.0)
+
+        self.render_number(self.timer)
+        self.after(1000, self.update_timer)
+
+    def render_number(self, timer):
+
+        pass
+
+    def update_timer(self):
+        self.timer -= 1
+        #if self.timer == 0:  <-0 될 시 어떻게 할지
 
 
-class TestRockPaperScissors(unittest.TestCase):
 
-    def createRockPaperScissorsUiFrame(self, rootWindow):
-        # ljs/test_notify_reader/test_motify_opponent_unit_spawn참고
-        def animate():
-            pre_drawed_battle_field_frame.redraw()
-            root.after(17, animate)
 
-        root.after(0, animate)
 
-        root.mainloop()
 
-    def test_rock_paper_scissors(self):
-        root = tkinter.Tk()
+class TestBattleFieldTimer(unittest.TestCase):
+
+    def test_time_frame(self):
+        root = tk.Tk()
+        root.title("TestTimer")
         root.geometry("1920x1080")
         root.configure(background="#151515")
-        self.createRockPaperScissorsUiFrame(root)
+        #app = battleFieldTimer(root)
+        app.pack(fill=tk.BOTH, expand=tk.YES)
+        app.animate = True
+        app.after(0, app.initgl)
         root.mainloop()
+
 
 
 if __name__ == '__main__':
     unittest.main()
+
+        #
+        # ljs/test_notify_function/test_motify_opponent_unit_spawn참고
+        # def animate():
+        #     pre_drawed_battle_field_frame.redraw()
+        #     root.after(17, animate)
+        #
+        # root.after(0, animate)
+        #
+        # root.mainloop()
