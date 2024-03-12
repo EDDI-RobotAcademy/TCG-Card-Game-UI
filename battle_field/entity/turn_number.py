dae76d6b7ec58f420842c01eb8b45228eba0ea1c
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2024-03-12 09:44:21 +0900

    update:[조승훈]배틀 필드에서 몇번 째 턴인지 표현하기[TCG-CGU-517]

---
+++ battle_field/entity/turn_number.py
@@ -0,0 +1,71 @@
+import math
+
+from battle_field.infra.your_field_energy_repository import YourFieldEnergyRepository
+from image_shape.circle_image import CircleImage
+from image_shape.non_background_image import NonBackgroundImage
+from image_shape.rectangle_image import RectangleImage
+from opengl_shape.rectangle import Rectangle
+from pre_drawed_image_manager.pre_drawed_image import PreDrawedImage
+
+
+
+class CurrentFieldEnergyRace:
+    __pre_drawed_image = PreDrawedImage.getInstance()
+    __your_field_energy_repository = YourFieldEnergyRepository.getInstance()
+
+    def __init__(self):
+        self.current_field_turn_number_panel = None
+
+        self.total_width = None
+        self.total_height = None
+
+        self.width_ratio = 1
+        self.height_ratio = 1
+
+    def set_total_window_size(self, width, height):
+        self.total_width = width
+        self.total_height = height
+
+    def get_width_ratio(self):
+        return self.width_ratio
+
+    def set_width_ratio(self, width_ratio):
+        self.width_ratio = width_ratio
+
+    def get_height_ratio(self):
+        return self.height_ratio
+
+    def set_height_ratio(self, height_ratio):
+        self.height_ratio = height_ratio
+
+    def change_local_translation(self, _translation):
+        self.local_translation = _translation
+
+    def get_current_field_turn_number_panel(self):
+        return self.current_field_turn_number_panel
+
+    def create_current_field_turn_number_panel(self):
+
+        left_x_point = self.total_width * 0.93
+        right_x_point = self.total_width * 0.97
+        top_y_point = self.total_height * 0.717
+        bottom_y_point = self.total_height * 0.757
+
+        self.current_field_energy_race_panel = NonBackgroundImage(
+            image_data=self.__pre_drawed_image.get_pre_draw_energy_race_with_race_number(
+                self.__your_field_energy_repository.get_current_field_energy_race().value),
+            vertices=[
+                (left_x_point, top_y_point),
+                (right_x_point, top_y_point),
+                (right_x_point, bottom_y_point),
+                (left_x_point, bottom_y_point)
+            ],
+            global_translation=(0, 0),
+            local_translation=(0, 0)
+        )
+
+    def update_current_field_turn_number_panel(self):
+        self.current_field_energy_race_panel.set_image_data(
+            self.__pre_drawed_image.get_pre_draw_energy_race_with_race_number(
+                self.__your_field_energy_repository.get_current_field_energy_race().value)
+        )
