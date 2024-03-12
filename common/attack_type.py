8faa95b01c67a26fe323f14415cff8fe6c7ca971
LoHenP <44047542+LoHenP@users.noreply.github.com>
2024-03-13 00:15:01 +0900

    Create attack_type.py

---
+++ common/attack_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class AttackType(Enum):
+    sword_attack = 0
+    magic_attack = 1
+    dark_blast = 2
+    moving_shadow_ball = 3
+    burst_shadow_ball = 4
+    death_scythe = 5
