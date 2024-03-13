42b4ae3698689a597d1cd786446ec0d935378d16
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-03-13 14:07:15 +0900

    update: [박장훈] fake battle field 에서 사운드가 재생됩니다 [TCG-CGS-31]

--- music_player/repository/music_player_repository_impl.py
+++ music_player/repository/music_player_repository_impl.py
@@ -2,8 +2,7 @@ import os
 import time
 
 import pygame.mixer
-from colorama import Fore, Style
-from pygame import mixer
+
 from music_player.repository.music_player_repository import MusicPlayerRepository
 
 
@@ -11,10 +10,14 @@ class MusicPlayerRepositoryImpl(MusicPlayerRepository):
     __instance = None
     __uiIpcChannel = None
     __backgroundMusicFilePathDict = {}
+    __soundPathDict = {}
 
     def __new__(cls):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
+
+            # pygame.mixer.init()
+
         return cls.__instance
 
     @classmethod
@@ -23,67 +26,49 @@ class MusicPlayerRepositoryImpl(MusicPlayerRepository):
             cls.__instance = cls()
         return cls.__instance
 
-    def loadBackgroundMusicPath(self, frame_name: str):
-        print(f"MusicPlayerRepositoryImpl : load_background_music - {frame_name}")
+    def saveUiIpcChannel(self, uiIpcChannel):
+        print('music channel saved')
+        self.__uiIpcChannel = uiIpcChannel
+
+    def load_background_music_path_with_frame_name(self, frame_name: str):
+        print(f"MusicPlayerRepositoryImpl : load_background_music_path_with_frame_name - {frame_name}")
         current_location = os.getcwd()
         background_music_path = os.path.join(current_location, 'local_storage', 'background_music', f'{frame_name}.mp3')
         self.__backgroundMusicFilePathDict[frame_name] = background_music_path
         print(self.__backgroundMusicFilePathDict)
 
-    def playBackgroundMusic(self):
-        pygame.mixer.init()
+    def load_sound_effect_path_with_event_name(self, event_name: str):
+        print(f"MusicPlayerRepositoryImpl : load_sound_effect_path_with_event_name - {event_name}")
+        current_location = os.getcwd()
+        sound_effect_file_path = os.path.join(current_location, 'local_storage', 'sound_effect', f'{event_name}.mp3')
+        self.__soundPathDict[event_name] = sound_effect_file_path
+        print(self.__soundPathDict)
 
+    def play_background_music(self):
         while True:
             try:
                 frame_name = self.__uiIpcChannel.get()
-                if frame_name is not None:
+                if frame_name:
                     for frame in self.__backgroundMusicFilePathDict.keys():
                         if frame == frame_name:
+                            path = self.__get_background_music_path(frame_name)
                             pygame.mixer.init()
-                            path = self.get_background_music_path(frame_name)
-                            self.playMusic(path)
+                            self.play_music(path)
+                            if pygame.mixer.music.get_busy():
+                                print("Background music - " + frame_name)
             finally:
                 time.sleep(0.1)
 
-    def get_background_music_path(self, frame_name: str):
-        print("MusicPlayerRepositoryImpl : get_background_music - " + frame_name)
-        return self.__backgroundMusicFilePathDict[frame_name]
-
-    def playMusic(self, path):
+    def play_music(self, path):
         pygame.mixer.music.load(path)
         pygame.mixer.music.play(-1)
-        if pygame.mixer.music.get_busy():
-            print("Music is playing!!")
 
-    # def changeBackgroundMusic(self):
-    #     self.__currentFrameName.pop(0)
-    #     mixer.music.stop()
-    #     mixer.quit()
-    #
-    # def getBackgroundMusicFilePath(self, frameName: str):
-    #     if self.__backgroundMusicFilePathDict[frameName]:
-    #         return self.__backgroundMusicFilePathDict[frameName]
-    #     else:
-    #         print(f"{Fore.RED}There is no background music for this frame{Style.RESET_ALL}")
-    #
-    # def detectFrameChange(self):
-    #     while True:
-    #         try:
-    #             frameName = self.__uiIpcChannel.get()
-    #             print(f"frame change detected: {frameName}")
-    #             print(f"current frame name is: {self.__currentFrameName}")
-    #             if not self.__currentFrameName:
-    #                 self.__currentFrameName.append(frameName)
-    #                 self.playBackgroundMusic()
-    #             elif frameName != self.__currentFrameName[0]:
-    #                 self.__currentFrameName.append(frameName)
-    #                 self.changeBackgroundMusic()
-    #                 self.playBackgroundMusic()
-    #             else:
-    #                 pass
-    #         finally:
-    #             time.sleep(0.5)
+    def __get_background_music_path(self, frame_name: str):
+        return self.__backgroundMusicFilePathDict[frame_name]
 
-    def saveUiIpcChannel(self, uiIpcChannel):
-        print('music channel saved')
-        self.__uiIpcChannel = uiIpcChannel
+    def play_sound_effect_with_event_name(self, event_name: str):
+        print("MusicPlayerRepositoryImpl : play_sound_effect_with_event_name - " + event_name)
+        sound = pygame.mixer.Sound(self.__soundPathDict[event_name])
+        sound.play()
+
+        time.sleep(0.1)
