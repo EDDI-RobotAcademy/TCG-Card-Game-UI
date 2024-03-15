4c6c84fe10335cdf0d07b6d9b0fe8f49b877f4b9
Sanghoon Lee <gcccompil3r@gmail.com>
2024-03-15 12:29:13 +0900

    feat: [이상훈] 네더 블레이드 생존 시 매 턴 발동하는 타겟팅 스킬을 본체에 사용 [TCG-CGU-564]

--- fake_battle_field/infra/fake_battle_field_frame_repository_impl.py
+++ fake_battle_field/infra/fake_battle_field_frame_repository_impl.py
@@ -83,3 +83,7 @@ class FakeBattleFieldFrameRepositoryImpl(FakeBattleFieldFrameRepository):
     def request_to_process_turn_start_first_passive_skill_to_your_field_unit(self, turnStartFirstPassiveSkillRequest):
         self.__transmitIpcChannel.put(turnStartFirstPassiveSkillRequest)
         return self.__receiveIpcChannel.get()
+
+    def request_to_process_turn_start_second_passive_skill_to_main_character(self, turnStartSecondPassiveSkillToMainCharacterRequest):
+        self.__transmitIpcChannel.put(turnStartSecondPassiveSkillToMainCharacterRequest)
+        return self.__receiveIpcChannel.get()
