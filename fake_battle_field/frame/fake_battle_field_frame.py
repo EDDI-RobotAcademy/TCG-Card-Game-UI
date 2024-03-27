02aed3abdd7ad3449c94111a9e4f983e91c91049
Sanghoon Lee <gcccompil3r@gmail.com>
2024-03-27 11:45:17 +0900

    update: don't need loading anymore [TCG-CGU]

--- fake_battle_field/frame/fake_battle_field_frame.py
+++ fake_battle_field/frame/fake_battle_field_frame.py
@@ -461,9 +461,9 @@ class FakeBattleFieldFrame(OpenGLFrame):
 
         self.attack_animation_object.set_total_window_size(self.width, self.height)
 
-        self.pre_drawed_image_instance.pre_draw_full_screen_nether_blade_skill(width, height)
-        self.pre_drawed_image_instance.pre_draw_full_screen_sea_of_wraith(width, height)
-        self.pre_drawed_image_instance.pre_draw_full_screen_nether_blade_targeting_skill(width, height)
+        # self.pre_drawed_image_instance.pre_draw_full_screen_nether_blade_skill(width, height)
+        # self.pre_drawed_image_instance.pre_draw_full_screen_sea_of_wraith(width, height)
+        # self.pre_drawed_image_instance.pre_draw_full_screen_nether_blade_targeting_skill(width, height)
 
         battle_field_scene = BattleFieldScene()
         battle_field_scene.create_battle_field_cene(self.width, self.height)
